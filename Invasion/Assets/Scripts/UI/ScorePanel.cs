295101fa065f9749482f6a0cd042f4c22b8276de
Nixonm <minigol5@hotmail.com>
2019-10-19 19:43:34 -0400

    Added death and touchdown functionnalities

---
+++ Invasion/Assets/Scripts/UI/ScorePanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScorePanel : MonoBehaviour
+{
+    [SerializeField]
+    protected TextMeshProUGUI m_text;
+
+    public void Instantiate(int greenScore, int redScore)
+    {
+        gameObject.SetActive(true);
+        m_text.text = greenScore.ToString() + " - " + redScore.ToString();
+        StartCoroutine(ClosePanel(3));
+
+    }
+
+    IEnumerator ClosePanel(float time)
+    {
+        yield return new WaitForSeconds(time);
+        gameObject.SetActive(false);
+    }
+}
