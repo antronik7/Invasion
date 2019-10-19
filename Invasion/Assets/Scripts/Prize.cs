1c914c3e01eb5e960a3fd2266dff4a7e01cc72f7
Nixonm <minigol5@hotmail.com>
2019-10-19 19:09:33 -0400

    Initial Max commit

---
+++ Invasion/Assets/Scripts/Prize.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prize : MonoBehaviour
+{
+    public void GetCaptured()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
