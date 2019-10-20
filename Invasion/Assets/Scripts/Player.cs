2f611ef7a3baab420ca8a9c618e5afd15d6bf09a
Nixonm <minigol5@hotmail.com>
2019-10-20 16:59:07 -0400

    COmmit for merge

--- Invasion/Assets/Scripts/Player.cs
+++ Invasion/Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     private bool m_isTurn;
     private Poque m_selectedPoque;
+    [SerializeField]
+    private ETeam m_team;
 
     public void StartTurn()
     {
@@ -13,8 +15,34 @@ public class Player : MonoBehaviour
         {
             if (m_selectedPoque != null)
             {
-                //Poque 
+                for (var i = 0; i < Input.touchCount; ++i)
+                {
+                    if (Input.GetTouch(i).phase == TouchPhase.Began)
+                    {
+                        RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position), Vector2.zero);
+                        // RaycastHit2D can be either true or null, but has an implicit conversion to bool, so we can use it like this
+                        if (hitInfo)
+                        {
+                            if (hitInfo.transform.GetComponent<Poque>())
+                            {
+                                TrySelectPoque(hitInfo.transform.GetComponent<Poque>());
+                            }
+                        }
+                    }
+                }
             }
+            else
+            {
+
+            }
+        }
+    }
+
+    void TrySelectPoque(Poque poque)
+    {
+        if (m_selectedPoque == null && m_isTurn && m_selectedPoque.GetTeam() == m_team)
+        {
+            m_selectedPoque = poque;
         }
     }
 }
--- Invasion/Assets/Scripts/Poque.cs
+++ Invasion/Assets/Scripts/Poque.cs
@@ -14,6 +14,8 @@ public class Poque : MonoBehaviour
     [SerializeField]
     protected ETeam m_team;
 
+    public ETeam GetTeam() { return m_team; }
+
     void Start()
     {
         m_initialPosition = transform.position;
