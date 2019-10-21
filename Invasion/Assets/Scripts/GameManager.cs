79dc0745467570df9edf6d477d47fcc5600072d6
Nixonm <minigol5@hotmail.com>
2019-10-20 20:06:06 -0400

    UI for player turns

--- Invasion/Assets/Scripts/GameManager.cs
+++ Invasion/Assets/Scripts/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private ScorePanel m_scorePanel;
     [SerializeField]
+    private PlayerTurnPanel m_playerTurnPanel;
+    [SerializeField]
     private int m_turnIndex;
     [SerializeField]
     private Player m_player1;
@@ -33,18 +35,19 @@ public class GameManager : MonoBehaviour
 
     public void StartTurn()
     {
-        Debug.Log("Start turn #" + m_turnIndex);
         m_turnIndex++;
         GameplayController.m_instance.AssignCharacterController(null);
         if (m_turnIndex % 2 == 1)
         {
             m_player2.EndTurn();
             m_player1.StartTurn();
+            m_playerTurnPanel.Instantiate(ETeam.Green);
         }
         else
         {
             m_player1.EndTurn();
             m_player2.StartTurn();
+            m_playerTurnPanel.Instantiate(ETeam.Red);
         }
     }
 
---
+++ Invasion/Assets/Scripts/UI/PlayerTurnPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerTurnPanel : MonoBehaviour
+{
+    [SerializeField]
+    protected TextMeshProUGUI m_text;
+
+    public void Instantiate(ETeam teamTurn)
+    {
+        gameObject.SetActive(true);
+        if (teamTurn == ETeam.Green)
+        {
+            m_text.text = "Green player's turn";
+        }
+        else if (teamTurn == ETeam.Red)
+        {
+            m_text.text = "Red player's turn";
+        }
+        StartCoroutine(ClosePanel(3));
+
+    }
+
+    IEnumerator ClosePanel(float time)
+    {
+        yield return new WaitForSeconds(time);
+        gameObject.SetActive(false);
+    }
+}
