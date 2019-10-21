39ea1d885082d5fa7311a9a9a2e2df20e8ebc9af
Nixonm <minigol5@hotmail.com>
2019-10-20 20:06:18 -0400

    Oops, rest of the ui

--- Invasion/Assets/Scripts/UI/PlayerTurnPanel.cs
+++ Invasion/Assets/Scripts/UI/PlayerTurnPanel.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerTurnPanel : MonoBehaviour
 {
     [SerializeField]
     protected TextMeshProUGUI m_text;
+    [SerializeField]
+    protected Color m_redColor;
+    [SerializeField]
+    protected Color m_greenColor;
 
     public void Instantiate(ETeam teamTurn)
     {
@@ -14,10 +19,12 @@ public class PlayerTurnPanel : MonoBehaviour
         if (teamTurn == ETeam.Green)
         {
             m_text.text = "Green player's turn";
+            GetComponent<Image>().color = m_greenColor;
         }
         else if (teamTurn == ETeam.Red)
         {
             m_text.text = "Red player's turn";
+            GetComponent<Image>().color = m_redColor;
         }
         StartCoroutine(ClosePanel(3));
 
